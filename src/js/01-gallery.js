53c7f1ae3d18d6b24ef10248d3b2a03c9ea87a8b
DenisMalv <kissed03@gmail.com>
2021-12-18 18:18:18 +0200

    Исправил баг в div были li

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -13,11 +13,11 @@ gallery.insertAdjacentHTML('beforeend', createGaleryMarkup())
 
 function createGaleryMarkup() {
     return galleryItems.map(({ preview, original, description }) =>    
-        `<li class="gallery__item">
+        `<div class="gallery__item">
             <a class="gallery__link" href="${original}">
                 <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
-        </li>`).join('')
+        </div>`).join('')
 }
 
 const lightBox = new SimpleLightbox('.gallery a', {
